1b420c5ddea9549ea8278526b7d7d8c7dfaafc0f
waqas sarwar <waqassarwarmuhammad@gmail.com>
2025-05-23 18:14:03 +0500

    fixed hero image

--- src/app/Hero.tsx
+++ src/app/Hero.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="relative w-full h-[105px] md:h-[348px] mt-10">
-      <Image src="/heroimg.png" alt="ombud" fill className="object-contain" />
+    <div className="relative  h-[105px] md:h-[348px] mt-10">
+      <Image src="/heroimg.png" alt="ombud" fill  />
     </div>
   );
 };
